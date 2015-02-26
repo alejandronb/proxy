6b8316a04d52396ddb0baef7c29b526746788418
Alejandro <ale90nb@gmail.com>
2015-02-26 09:14:50 +0100

    Prueba de otro forma para el borrado

--- script-proxy.py
+++ script-proxy.py
@@ -9,10 +9,12 @@ if opcion == "-a":
 		fichero = open("/etc/squid3/urlnegro.acl","a")
 		fichero.write(contenido+"\n")
 		fichero.close
+		print "La url se ha añadido"
 	elif tipo == "-dom":
 		fichero = open("/etc/squid3/domnegro.acl","a")
 		fichero.write(contenido+"\n")
 		fichero.close
+		print "El dominio se ha añadido"
 elif opcion == "-d":
 	if tipo == "-url":
 		fichero = open("/etc/squid3/urlnegro.acl","r")
@@ -27,13 +29,32 @@ elif opcion == "-d":
 				fichero.close
 				#Comprobar funcionamiento
 	elif tipo == "-dom":
-		fichero = open("/etc/squid/domnegro.acl","r")
+		fichero = open("/etc/squid3/domnegro.acl","r")
 		lineas = fichero.readlines()
 		fichero.close
 		for linea in lineas:
-			if linea == contenido:
+			if linea.find(contenido) == 0:
+				linea = ""
 				fichero = open("/etc/squid3/domnegro","w")
-				lineas.remove(contenido)
 				for linea in lineas:
 					fichero.write(linea)
 				fichero.close
+
+
+
+
+
+
+
+
+	# elif tipo == "-dom":
+	# 	fichero = open("/etc/squid3/domnegro.acl","r")
+	# 	lineas = fichero.readlines()
+	# 	fichero.close
+	# 	for linea in lineas:
+	# 		if linea == contenido:
+	# 			lineas.remove(contenido)
+	# 			fichero = open("/etc/squid3/domnegro","w")
+	# 			for linea in lineas:
+	# 				fichero.write(linea)
+	# 			fichero.close
